8c7e9649989e977e3783d3b2c2d42621ede41077
Mostafa229Gh <Mostafa229Gh@gmail.com>
2024-11-24 19:32:37 +0330

    add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors:{
+        'Charcoal-Blue':'#121A21',
+        'Pale-Mint':'#F1F8F5',
+        'Gunmetal':'#2C3E50'
+      }
+    },
+    fontFamily: {
+      'Audiowide':['Audiowide'],
+      'Anta':['Anta']
+    }
+  },
+  plugins: [],
+};
