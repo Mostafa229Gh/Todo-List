7656d4ad60d328e138427420a9700904b5f9dc0e
Mostafa229Gh <Mostafa229Gh@gmail.com>
2024-12-19 22:23:14 +0330

    Change structure of code

--- src/Components/NewTask.js
+++ src/Components/NewTask.js
@@ -1,19 +1,42 @@
 import React, { useState } from "react";
-import PriorityOptions from "./PriorityOptions";
-import DeadlineOption from "./DeadlineOption";
 
 function NewTask() {
-  const [selectedTaskOption, setSelectedTaskOption] = useState(""); // Tracks both task option and priority
+  const [selectedTaskOption, setSelectedTaskOption] = useState("");
+  const [dateTime, setDateTime] = useState("");
+  const [selectedPriority, setSelectedPriority] = useState("");
+  const [selectedColor, setSelectedColor] = useState("");
+
+  const priorities = ["Later", "Regular", "Important", "Essential"];
+  const colors = [
+    { name: "#90B9E0", className: "bg-Sky-Blue" },
+    { name: "#9BEA96", className: "bg-Mint-Green" },
+    { name: "#E7C3C3", className: "bg-Dusty-Rose" },
+    { name: "#E9D889", className: "bg-Golden-Sand" },
+    { name: "#FACA96", className: "bg-Apricot" },
+  ];
 
   const handleOptionChange = (value) => {
     setSelectedTaskOption(value);
+  };
+
+  const handleDateTimeChange = (event) => {
+    setDateTime(event.target.value);
+  };
+
+  const handlePriorityChange = (priority) => {
+    setSelectedPriority(priority);
+  };
+
+  const handleColorClick = (color) => {
+    setSelectedColor(color);
+    console.log(color);
   };
 
   return (
     <div
       className="
         w-80 sm:w-1/2 
-        h-3/4 sm:h-4/5 
+        h-[78%] sm:h-4/5 
         px-3.5 py-4 
         fixed top-1/2 left-1/2 -translate-x-1/2 
         -translate-y-1/2 sm:-translate-y-[55%] 
@@ -25,6 +48,7 @@ function NewTask() {
         action=""
         className="h-full flex flex-col space-y-4 font-Roboto text-Gunmetal"
       >
+        {/* Title input */}
         <div>
           <input
             type="text"
@@ -35,6 +59,7 @@ function NewTask() {
           />
         </div>
 
+        {/* Description input */}
         <div>
           <label htmlFor="description" className="block text-sm text-Gunmetal">
             Description
@@ -46,6 +71,7 @@ function NewTask() {
           ></textarea>
         </div>
 
+        {/* Choose one input */}
         <div className="text-Gunmetal">
           <p className="text-sm">Choose one:</p>
           <div className="space-y-2 mt-2">
@@ -54,7 +80,7 @@ function NewTask() {
                 type="radio"
                 name="taskOption"
                 value="Setting Priorities"
-                checked={selectedTaskOption.startsWith("Setting Priorities")}
+                checked={selectedTaskOption === "Setting Priorities"}
                 onChange={(e) => handleOptionChange(e.target.value)}
                 className="form-checkbox"
                 required
@@ -76,24 +102,59 @@ function NewTask() {
           </div>
         </div>
 
-        {selectedTaskOption.startsWith("Setting Priorities") && (
-          <PriorityOptions
-            selectedPriority={selectedTaskOption
-              .replace("Setting Priorities:", "")
-              .trim()}
-            onPriorityChange={(priority) =>
-              handleOptionChange(`Setting Priorities: ${priority}`)
-            }
-          />
+        {selectedTaskOption === "Setting Priorities" && (
+          <div className="flex flex-col items-center gap-1 pt-2 sm:pt-4">
+            {priorities.map((priority) => (
+              <label key={priority} className="priorities">
+                <input
+                  type="radio"
+                  name="priorityOption"
+                  value={priority}
+                  checked={selectedPriority === priority}
+                  onChange={() => handlePriorityChange(priority)}
+                  className="form-checkbox"
+                  required
+                />
+                <span className="ml-2 text-sm">{priority}</span>
+              </label>
+            ))}
+            <div className="mt-5 flex gap-2">
+              {colors.map(({ name, className }) => (
+                <button
+                  type="button"
+                  key={name}
+                  className={`w-10 h-10 rounded-full ${className} shadow-form ${
+                    selectedColor === name
+                      ? "shadow-none ring-1 ring-Charcoal-Blue "
+                      : ""
+                  }`}
+                  onClick={() => handleColorClick(name)}
+                  style={{ cursor: "pointer" }}
+                ></button>
+              ))}
+            </div>
+          </div>
         )}
-        {(selectedTaskOption === "Establishing a Deadline") && (
-          <DeadlineOption/>
+
+        {selectedTaskOption === "Establishing a Deadline" && (
+          <div className="flex flex-col items-center pt-8">
+            <label htmlFor="datetime" className="text-sm">
+              Select Date and Time:
+            </label>
+            <input
+              type="datetime-local"
+              id="datetime"
+              value={dateTime}
+              onChange={handleDateTimeChange}
+              className="p-2 border border-Pale-Mint rounded-lg"
+            />
+          </div>
         )}
 
         <div className="flex flex-col flex-grow justify-end mt-4">
           <button
             type="submit"
-            className="h-8 w-28 sm:w-36 bg-Charcoal-Blue text-white rounded-md self-center"
+            className="fixed h-8 w-28 sm:w-36 bg-Charcoal-Blue text-white rounded-md self-center"
           >
             Add
           </button>
